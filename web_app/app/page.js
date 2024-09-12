7bd9306fc017d86291bcc3edb4870b0f235e0f90
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-09-13 01:18:10 +0530

    Update

---
+++ web_app/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Detector } from "@/components/component/detector";
+
+export default function Home() {
+  return (
+    <div>
+     
+      <Detector />
+    </div>
+  );
+    
+}
