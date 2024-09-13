9a3afa38ccb02bae6f450b4b0b0e695413fa7b22
Shreyas <Shreyas.Biradar@iiitb.ac.in>
2024-09-13 11:27:09 +0530

    commit about_to_submit

---
+++ web_app/app/Home/page.js
@@ -0,0 +1,13 @@
+import { HomePage } from "@/components/component/home-page";
+
+
+
+export default function Home() {
+    return (
+      <div>
+       
+        <HomePage />
+      </div>
+    );
+      
+  }
